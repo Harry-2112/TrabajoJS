404a1fc81d1d0738a856f4cb10539de0e92da882
Harry211201 <116766787+Harry211201@users.noreply.github.com>
2022-11-15 15:59:12 -0500

    cambio 7

--- docs/7/main.js
+++ docs/7/main.js
@@ -3,18 +3,18 @@ function calcular() {
     let error = document.getElementById("error");
     let total = document.getElementById("total");
 
-    if (numero.length != 3) {
-        error.innerHTML = "Ingrese un numero de 3 digitos";
-    } else {
+    if (numero.length == 3) {
         let numero1 = parseInt(numero.charAt(0));
         let numero2 = parseInt(numero.charAt(1));
         let numero3 = parseInt(numero.charAt(2));
 
-        let total1=numero1**2;
-        let total2=numero2**2;
-        let total3=numero3**2;
-        let suma=total1+total2+total3;
-        total.innerHTML=suma;
+        let total1 = numero1 ** 2;
+        let total2 = numero2 ** 2;
+        let total3 = numero3 ** 2;
+        let suma = total1 + total2 + total3;
+        total.innerHTML = suma;
+    } else {
+        error.innerHTML = "Ingrese un numero de 3 digitos";
     }
 
 }
